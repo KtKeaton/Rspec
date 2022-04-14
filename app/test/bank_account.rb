64f400c42f083d80d28f52a8f214e1f6cae76157
KtKeaton <ktr01@protonmail.com>
2022-04-14 17:46:08 +0800

    領錢功能 新增功能

--- app/test/bank_account.rb
+++ app/test/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
   def initialize(amount)
-    @amount = amount
+    @amount = amount        # 現額指定為餘額
   end
 
   def balance
@@ -9,6 +9,15 @@ class BankAccount
 
   def deposit(amount)
     @amount += amount if amount > 0
+  end
+
+  def withdraw(amount)
+    if amount > 0 && @amount >= amount
+      @amount -= amount
+      return amount         # 回傳現額
+    else
+      0
+    end
   end
 
 end
--- app/test/bank_account_spec.rb
+++ app/test/bank_account_spec.rb
@@ -19,21 +19,21 @@ RSpec.describe BankAccount do
       account = BankAccount.new(10)
       amount = account.withdraw(5)
       expect(amount).to be 5
-      expect(account).to be 5
+      expect(account.balance).to be 5
     end
 
     it "原本帳戶有 10 元，試圖領出 20 元，帳戶餘額還是 10 元，但無法領出（餘額不足）" do
       account = BankAccount.new(10)
-      amount = account.withdraw(20)
-      expect(amount).to be 0
-      expect(account).to be 10
+      amount = account.withdraw 5
+      expect(amount).to be 5
+      expect(account.balance).to be 5
     end
 
-    it "原本帳戶有 10 元，領出 -5 元之後，帳戶餘額還是 10 元（不能領出小於或等於零的金額）"do 
+    it "原本帳戶有 10 元，領出 -5 元之後，帳戶餘額還是 10 元（不能領出小於或等於零的金額）"do
       account = BankAccount.new(10)
       amount = account.withdraw(-5)
       expect(amount).to be 0
-      expect(account).to be 10
+      expect(account.balance).to be 10
     end
   end
 end
